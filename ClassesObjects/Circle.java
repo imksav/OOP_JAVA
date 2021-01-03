ea2309100af2a971b92f61a34050a187b4ff1ef9
Keshav Bhandari <imksav@gmail.com>
2021-01-03 21:33:53 +0545

    Added circle

---
+++ ClassesObjects/Circle.java
@@ -0,0 +1,46 @@
+
+/*
+Write a class Circle with a parameterized constructor. If no parameters are passed then its default constructor should be invoked parameterized constructor with default values. A circle is defined using radius and circumference.
+*/
+import java.util.Scanner;
+
+public class Circle {
+     private double radius;
+
+     Circle(double radius) {
+          this.radius = radius;
+     }
+
+     Circle() {
+          this.radius = 0;
+     }
+
+     // setter and getter
+     // setter
+     public void setRadius(double radius) {
+          this.radius = radius;
+     }
+
+     // getter
+     public double getRadius() {
+          return this.radius;
+     }
+
+     // methods
+     public void displayCircumference() {
+          double circumference = 2 * (22 / 7) * radius;
+          System.out.println("Circumference of a circle is: " + circumference);
+     }
+
+     public static void main(String[] args) {
+          Scanner sc = new Scanner(System.in);
+          System.out.println("Enter value of radius:");
+          double radius = sc.nextDouble();
+          Circle c1 = new Circle(radius);
+          Circle c2 = new Circle();
+          c1.displayCircumference();
+          c2.displayCircumference();
+
+          sc.close();
+     }
+}
