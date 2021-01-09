a35d7335a46b640d15968f45639f3a4780fea66a
Keshav Bhandari <imksav@gmail.com>
2021-01-09 17:10:04 +0545

    Added complete code

--- Arrays/QN1.java
+++ Arrays/QN1.java
@@ -2,15 +2,15 @@
 Write a Java Method to input n integer values from user, store it into an array and print all the elements.
 */
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class QN1 {
 
      static void print(int n, int Integer[]) {
-          System.out.println("===============Printing the " + n + " integer value you have entered===============");
-          for (int i = 0; i < n; i++) {
-               System.out.print(Integer[i] + "\t");
-          }
+          System.out.println(
+                    "===========Printing the " + n + " integer value that you have entered in array===========");
+          System.out.println(Arrays.toString(Integer));
      }
 
      public static void main(String[] args) {
