d1289e8549201b4be5019be775009262fdeba4e1
Keshav Bhandari <imksav@gmail.com>
2021-01-09 17:24:42 +0545

    Added QN3

--- Arrays/QN3.java
+++ Arrays/QN3.java
@@ -2,31 +2,30 @@
 Write a Java Method to test if an array contains a specific value. (Linear Search).
 */
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class QN3 {
      static boolean found = false;
 
-     static void linearSearch(int array[], int searchNum) {
+     static boolean linearSearch(int array[], int searchNum) {
           int len = array.length;
           for (int i = 0; i < len; i++) {
                if (array[i] == searchNum) {
                     found = true;
+                    break;
                }
           }
-          if (found) {
-               System.out.println(searchNum + " is found in the array.");
-          } else {
-               System.out.println(searchNum + " is not found in the array.");
-          }
+          return found;
      }
 
      public static void main(String[] args) {
           Scanner sc = new Scanner(System.in);
           int array[] = { 1, 3, 5, 7, 9, 11, 13, 15, 17, 19 };
+          System.out.println("Initial array: " + Arrays.toString(array));
           System.out.println("Enter a value you want to search in the array: ");
           int searchNum = sc.nextInt();
-          linearSearch(array, searchNum);
+          System.out.println("Array has value?\nAnswer: " + linearSearch(array, searchNum));
           sc.close();
      }
 }
