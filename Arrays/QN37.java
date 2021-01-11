42e4760c1d23784ff9e6495b3d6d01688e5c7402
Keshav Bhandari <imksav@gmail.com>
2021-01-11 23:04:28 +0545

    Added QN37

---
+++ Arrays/QN37.java
@@ -0,0 +1,28 @@
+/*
+Write a Java Method to find maximum difference between two elements in a given array of integers such that smaller element appears before larger element.   
+Example:
+     Input :
+     nums = { 2, 3, 1, 7, 9, 5, 11, 3, 5 }
+     Output:
+     The maximum difference between two elements of the said array elements 10
+*/
+
+public class QN37 {
+     public static int getMaxDifferece(int[] arr) {
+          int maxDiff = 0;
+          for (int i = 0; i < arr.length; i++) {
+               for (int j = i; j < arr.length; j++) {
+                    if (arr[j] - arr[i] > maxDiff) {
+                         maxDiff = arr[j] - arr[i];
+                    }
+               }
+          }
+          return maxDiff;
+     }
+
+     public static void main(String[] args) {
+          int[] arr = { 2, 3, 1, 7, 9, 5, 11, 3, 5 };
+          int maxDifference = getMaxDifferece(arr);
+          System.out.println("The maximum difference is: " + maxDifference);
+     }
+}
