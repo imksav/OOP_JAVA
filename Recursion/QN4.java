573289f404640de3052ab456097d780078c51cdc
Keshav Bhandari <imksav@gmail.com>
2021-01-12 22:56:29 +0545

    Added QN4

---
+++ Recursion/QN4.java
@@ -0,0 +1,27 @@
+
+/*
+The fibonacci sequence is a famous bit of mathematics, and it happens to have a recursive definition. The first two values in the sequence are 0 and 1 (essentially 2 base cases). Each subsequent value is the sum of the previous two values, so the whole sequence is: 0, 1, 1, 2, 3, 5, 8, 13, 21 and so on. Define a recursive fibonacci(n) method that returns the nth fibonacci number, with n=0 representing the start of the sequence. 
+*/
+import java.util.Scanner;
+
+public class QN4 {
+
+     public static int fibonacci(int n) {
+          if (n == 0) {
+               return 0;
+          } else if (n == 1) {
+               return 1;
+          } else
+               return fibonacci(n - 1) + fibonacci(n - 2);
+     }
+
+     public static void main(String[] args) {
+
+          Scanner sc = new Scanner(System.in);
+          System.out.println(" Enter the natural number to n terms :");
+          int num = sc.nextInt();
+          System.out.println("The sum of fibonacci number upto " + num + " term = " + fibonacci(num));
+          sc.close();
+     }
+
+}
